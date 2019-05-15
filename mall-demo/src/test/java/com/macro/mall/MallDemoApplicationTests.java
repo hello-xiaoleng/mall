cedccdfb31b5645ddde1c051fe0cf0b27856800a
chenjia <chenjia@dongfangfuli.com>
2019-05-15 17:21:35 +0800

    project dependency management

--- mall-demo/src/test/java/com/macro/mall/MallDemoApplicationTests.java
+++ mall-demo/src/test/java/com/macro/mall/MallDemoApplicationTests.java
@@ -4,15 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.macro.mall.model.PmsProduct;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MallDemoApplicationTests {
-	private Logger logger = LoggerFactory.getLogger(MallDemoApplicationTests.class);
+
 	@Test
 	public void contextLoads() {
 	}
@@ -24,8 +22,6 @@ public class MallDemoApplicationTests {
 		product.setId(1L);
 		product.setName("小米手机");
 		product.setBrandName("小米");
-		logger.info(mapper.writeValueAsString(product));
-		logger.error(mapper.writeValueAsString(product));
 	}
 
 }
